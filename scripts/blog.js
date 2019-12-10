ca3096d13cec97013653cded2ca97f690fde9859
Li Xuetao <lixuetaoleo@163.com>
2019-12-10 17:43:37 +0800

    图片自适应多种设备

---
+++ scripts/blog.js
@@ -0,0 +1,11 @@
+// let bgi = document.getElementById("bgi");
+
+// let width = window.innerWidth;
+// let height = window.innerHeight;
+// console.log(width);
+// console.log(height);
+// window.onload = function(){
+//     bgi.setAttribute("height",height);
+// bgi.setAttribute("width",width);
+// }
+
