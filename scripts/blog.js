493ae446fd6ea2afbc31edd293e0866274797601
Li Xuetao <lixuetaoleo@163.com>
2019-12-13 17:16:45 +0800

    添加了contac和about页面的HTML文件

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,14 +1,14 @@
 let bgi = document.getElementById("backgroundImage");
 var nthimg = 0;
 window.onload = function(){
-    bgi.style.backgroundImage = "url(img1.jpg)";
+    bgi.style.backgroundImage = "url(./images/img1.jpg)";
 }
 
 setInterval(changeImg, 4000);
 
 function changeImg(){
     console.log("changeimg")
-    bgi.style.backgroundImage = 'url(img'+nthimg+'.jpg)';
+    bgi.style.backgroundImage = 'url(./images/img'+nthimg+'.jpg)';
     bgi.style.transitionDuration = "1s"
     nthimg = (++nthimg) % 2;
 }
